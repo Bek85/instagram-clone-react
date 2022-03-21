ba2802018768ac311f8ceb31372f50cd48312c5d
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-03-21 18:08:59 +0500

    create and style <FeedSideSuggestions/> component

--- src/components/feed/FeedSideSuggestions.jsx
+++ src/components/feed/FeedSideSuggestions.jsx
@@ -1,9 +1,34 @@
-import { useFeedSideSuggestionsStyles } from '../../styles';
+import { useFeedSideSuggestionsStyles } from '@/styles';
+import { Paper, Typography } from '@mui/material';
+import { getDefaultUser } from '@/data';
+import UserCard from '@/components//shared/UserCard';
+import FollowButton from '@/components/shared/FollowButton';
 
 function FeedSideSuggestions() {
-  useFeedSideSuggestionsStyles();
+  const classes = useFeedSideSuggestionsStyles();
 
-  return <div>FeedSideSuggestions</div>;
+  return (
+    <article className={classes.article}>
+      <Paper className={classes.paper}>
+        <Typography
+          color="textSecondary"
+          variant="subtitle2"
+          component="h2"
+          align="left"
+          gutterBottom
+          className={classes.typography}
+        >
+          Suggestions For You
+        </Typography>
+        {Array.from({ length: 5 }, () => getDefaultUser()).map((user) => (
+          <div key={user.id} className={classes.card}>
+            <UserCard user={user} />
+            <FollowButton side />
+          </div>
+        ))}
+      </Paper>
+    </article>
+  );
 }
 
 export default FeedSideSuggestions;
--- src/components/shared/FollowButton.jsx
+++ src/components/shared/FollowButton.jsx
@@ -1,9 +1,36 @@
-import { useFollowButtonStyles } from '../../styles';
+import { useFollowButtonStyles } from '@/styles';
+import { useState } from 'react';
+import Button from '@mui/material/Button';
 
-function FollowButton() {
-  useFollowButtonStyles();
+function FollowButton({ side }) {
+  const classes = useFollowButtonStyles({ side });
+  const [isFollowing, setFollowing] = useState(false);
 
-  return <div>FollowButton</div>;
+  const followButton = (
+    <Button
+      variant={side ? 'text' : 'contained'}
+      color="primary"
+      className={classes.button}
+      onClick={() => setFollowing(true)}
+      fullWidth
+    >
+      Follow
+    </Button>
+  );
+
+  const followingButton = (
+    <Button
+      variant={side ? 'text' : 'outlined'}
+      color="secondary"
+      className={classes.button}
+      onClick={() => setFollowing(false)}
+      fullWidth
+    >
+      Following
+    </Button>
+  );
+
+  return isFollowing ? followingButton : followButton;
 }
 
 export default FollowButton;
